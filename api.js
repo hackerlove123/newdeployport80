61ee5837178f80bf3b265d4ffdf2026c22efe9be
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-02-10 23:52:43 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { exec } = require("child_process");
 const app = express();
-const port = 9999;
+const port = 80;
 const MAX_CONCURRENT_ATTACKS = 1;
 
 let activeAttacks = 0;
